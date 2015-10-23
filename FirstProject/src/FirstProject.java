9470be414b8c5e8da6affcf5cc934564f04595d9
Rebecca Nicacio <rebecca_nicacio@yahoo.com>
2015-10-23 16:21:55 -0500

    Update, added scanner for user input

--- FirstProject/src/FirstProject.java
+++ FirstProject/src/FirstProject.java
@@ -1,6 +1,38 @@
-
+import java.util.*;
 public class FirstProject {
 	public static void main(String[] args){
-		System.out.println("Hello World!");
+		Scanner console = new Scanner(System.in);
+		System.out.println("Type a number from 1-100: ");
+		int number = console.nextInt();
+		if(number > 100){
+			System.out.println("Please input a number between 1 and 100");
+			number = console.nextInt();
+			factor(number, console);
+		} else{
+			factor(number, console);
+			
+		}
+		
+	}
+	
+	public static void factor(int number, Scanner console){
+		for (int i = 1; i < number; i++){
+			if(number % i == 0){
+				System.out.print(i + " ");
+			}
+		}
+		System.out.print(number);
+		quit(number, console);
+	}
+	
+	public static void quit(int number, Scanner console){
+		System.out.print("\nQuit?");
+		String quit = console.next();
+		if(quit.toLowerCase().equals("quit") || quit.toLowerCase().equals("yes") || quit.toLowerCase().equals("y")){
+			
+		} else if (quit.toLowerCase().equals("no") || quit.toLowerCase().equals("n")){ 
+			number = console.nextInt();
+			factor(number, console); 
+		}
 	}
 }
