38634d6c74a2a8ea4dc1eabab7ec15f2380bd368
Rebecca Nicacio <rebecca_nicacio@yahoo.com>
2015-10-29 15:24:25 -0500

    Added try/catch for an exception. Need to find out how to keep the code running after catching the exception

--- FirstProject/src/FirstProject.java
+++ FirstProject/src/FirstProject.java
@@ -2,37 +2,40 @@ import java.util.*;
 public class FirstProject {
 	public static void main(String[] args){
 		Scanner console = new Scanner(System.in);
-		System.out.println("Type a number from 1-100: ");
-		int number = console.nextInt();
-		if(number > 100){
-			System.out.println("Please input a number between 1 and 100");
-			number = console.nextInt();
-			factor(number, console);
-		} else{
-			factor(number, console);
-			
-		}
+		factor(console);
 		
 	}
 	
-	public static void factor(int number, Scanner console){
-		for (int i = 1; i < number; i++){
-			if(number % i == 0){
-				System.out.print(i + " ");
+	public static void factor(Scanner console){
+		System.out.println("Type a number from 1-100: ");
+		int number;
+		try{
+			number = console.nextInt();
+			if(number > 100){
+				System.out.println("Please input a number between 1 and 100 ");
+				number = console.nextInt();
+			} else{
+				for (int i = 1; i < number; i++){
+					if(number % i == 0){
+						System.out.print(i + " ");
+					}
+				}
 			}
+			System.out.print(number);
+			quit(number, console);
+		} catch(InputMismatchException e){
+			System.out.println("Only integer values allowed");
+			
 		}
-		System.out.print(number);
-		quit(number, console);
 	}
 	
 	public static void quit(int number, Scanner console){
-		System.out.print("\nQuit?");
+		System.out.print("\nQuit? ");
 		String quit = console.next();
 		if(quit.toLowerCase().equals("quit") || quit.toLowerCase().equals("yes") || quit.toLowerCase().equals("y")){
 			
 		} else if (quit.toLowerCase().equals("no") || quit.toLowerCase().equals("n")){ 
-			number = console.nextInt();
-			factor(number, console); 
+			factor(console); 
 		}
 	}
 }
