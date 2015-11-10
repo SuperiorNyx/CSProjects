0c1ce4bad2e73d2af154a1945dc28bfc2c4222c5
Rebecca Nicacio <rebecca_nicacio@yahoo.com>
2015-11-10 16:29:13 -0600

    Exception catch works, one bug is left

--- FirstProject/src/FirstProject.java
+++ FirstProject/src/FirstProject.java
@@ -2,40 +2,58 @@ import java.util.*;
 public class FirstProject {
 	public static void main(String[] args){
 		Scanner console = new Scanner(System.in);
-		factor(console);
+		//int number = 0;
+		System.out.println("Type a number from 1-100: ");
+		checkMismatch(console);
+		
+	}
+	public static void checkMismatch(Scanner console){
+
+		int number = 0;
+		boolean retry = true;
+		do{
+			try{
+				number = Integer.parseInt(console.nextLine());
+				if(number > 100 || number < 1){
+					System.out.println("Please input a number between 1 and 100: ");
+					number = console.nextInt();
+					factor(console, number);
+				} else {
+					factor(console, number);
+				}
+				retry = true;
+			} catch(NumberFormatException e){
+				System.out.println("Only integer values allowed.");
+				checkMismatch(console);
+			
+			}
+		} while (!retry);
 		
 	}
 	
-	public static void factor(Scanner console){
-		System.out.println("Type a number from 1-100: ");
-		int number;
-		try{
-			number = console.nextInt();
-			if(number > 100){
-				System.out.println("Please input a number between 1 and 100 ");
-				number = console.nextInt();
-			} else{
-				for (int i = 1; i < number; i++){
-					if(number % i == 0){
-						System.out.print(i + " ");
-					}
+	public static void factor(Scanner console, int number){
+		
+		
+			for (int i = 1; i < number; i++){
+				if(number % i == 0){
+					System.out.print(i + " ");
 				}
 			}
 			System.out.print(number);
-			quit(number, console);
-		} catch(InputMismatchException e){
-			System.out.println("Only integer values allowed");
-			
-		}
+			quit(console);
+		
+		
+		
 	}
 	
-	public static void quit(int number, Scanner console){
+	public static void quit(Scanner console){
 		System.out.print("\nQuit? ");
 		String quit = console.next();
 		if(quit.toLowerCase().equals("quit") || quit.toLowerCase().equals("yes") || quit.toLowerCase().equals("y")){
 			
 		} else if (quit.toLowerCase().equals("no") || quit.toLowerCase().equals("n")){ 
-			factor(console); 
+			System.out.println("Type a number from 1-100: ");
+			checkMismatch(console); 
 		}
 	}
 }
