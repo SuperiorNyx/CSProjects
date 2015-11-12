0cce27fb68f0d9e18430b29013219234d567130e
Rebecca Nicacio <rebecca_nicacio@yahoo.com>
2015-11-12 15:20:51 -0600

    Program can do the tasks required by Part 1 of the assignment

--- FirstProject/src/FirstProject.java
+++ FirstProject/src/FirstProject.java
@@ -2,58 +2,46 @@ import java.util.*;
 public class FirstProject {
 	public static void main(String[] args){
 		Scanner console = new Scanner(System.in);
-		//int number = 0;
 		System.out.println("Type a number from 1-100: ");
-		checkMismatch(console);
+		int number = console.nextInt();
+		checkMismatch(console, number);
 		
 	}
-	public static void checkMismatch(Scanner console){
-
-		int number = 0;
-		boolean retry = true;
-		do{
-			try{
-				number = Integer.parseInt(console.nextLine());
-				if(number > 100 || number < 1){
-					System.out.println("Please input a number between 1 and 100: ");
-					number = console.nextInt();
-					factor(console, number);
-				} else {
-					factor(console, number);
-				}
-				retry = true;
-			} catch(NumberFormatException e){
-				System.out.println("Only integer values allowed.");
-				checkMismatch(console);
-			
+	
+	public static void checkMismatch(Scanner console, int number){
+		try{
+			if(number > 100 || number < 1){
+				System.out.println("Please input a number between 1 and 100: ");
+				number = console.nextInt();
+				factor(console, number);
+			} else {
+				factor(console, number);
 			}
-		} while (!retry);
-		
+		} catch(InputMismatchException e){
+			System.out.println("Only integer values allowed.");
+			checkMismatch(console, number);
+		}
 	}
 	
 	public static void factor(Scanner console, int number){
-		
-		
-			for (int i = 1; i < number; i++){
-				if(number % i == 0){
-					System.out.print(i + " ");
-				}
+		for (int i = 1; i < number; i++){
+			if(number % i == 0){
+				System.out.print(i + " ");
 			}
-			System.out.print(number);
-			quit(console);
-		
-		
-		
+		}
+		System.out.print(number);
+		quit(console, number);
 	}
 	
-	public static void quit(Scanner console){
+	public static void quit(Scanner console, int number){
 		System.out.print("\nQuit? ");
 		String quit = console.next();
-		if(quit.toLowerCase().equals("quit") || quit.toLowerCase().equals("yes") || quit.toLowerCase().equals("y")){
+		if(quit.substring(0,1).toLowerCase().equals("q") || quit.substring(0,1).toLowerCase().equals("y")){
 			
-		} else if (quit.toLowerCase().equals("no") || quit.toLowerCase().equals("n")){ 
+		} else if (quit.substring(0,1).toLowerCase().equals("n")){ 
 			System.out.println("Type a number from 1-100: ");
-			checkMismatch(console); 
+			number = console.nextInt();
+			checkMismatch(console, number); 
 		}
 	}
 }
