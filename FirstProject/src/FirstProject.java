3d4a18e1a4fe69bcd62035f6f1aa36348f5865c4
Rebecca Nicacio <rebecca_nicacio@yahoo.com>
2015-11-16 15:00:45 -0600

    Program will check again if the user enters the wrong input more than once.

--- FirstProject/src/FirstProject.java
+++ FirstProject/src/FirstProject.java
@@ -3,56 +3,55 @@ public class FirstProject {
 	public static void main(String[] args){
 		Scanner console = new Scanner(System.in);
 		System.out.println("Type a number from 1-100: ");
-		int number = console.nextInt();
-	    System.out.println("Do you want to put in a second number? ");
-	    String yn = console.next();
-	    if(yn.substring(0,1).equals("y")){
-	      System.out.println("Type a number from 1-100: ");
-	         int number2 = console.nextInt();
-	         gcd(number, number2);
-	      } else {
-	         System.out.println("Factors of " + number);
-			   checkMismatch(console, number);
-	      }
+		int number = 0;
+		checkMismatch(console, number);	
 	}
 	
 	public static void checkMismatch(Scanner console, int number){
 		try{
+			number = Integer.parseInt(console.next());
 			if(number > 100 || number < 1){
 				System.out.println("Please input a number between 1 and 100: ");
-				number = console.nextInt();
-				factor(console, number);
+				checkMismatch(console, number);
 			} else {
-				factor(console, number);
+				factor(number);
+				quit(console, number);
 			}
-		} catch(InputMismatchException e){
-			System.out.println("Only integer values allowed.");
+		} catch(NumberFormatException e){
+			System.out.println("Only integer values allowed. Please input a number between 1 and 100: ");
 			checkMismatch(console, number);
 		}
 	}
 	
-	public static void factor(Scanner console, int number){
+	public static void factor(int number){
 		for (int i = 1; i < number; i++){
 			if(number % i == 0){
 				System.out.print(i + " ");
 			}
 		}
 		System.out.print(number);
-		quit(console, number);
 	}
 	
-	public static void gcd(int number, int number2){
-		
+	public static int gcd(int number, int number2){
+		int biggest = number;
+		int smallest = number2;
+		if(number2>number){
+			biggest = number2; smallest = number; 
+		}
+		if(smallest==0){
+			return biggest; 
+		} else {
+			return gcd(smallest, biggest%smallest);
+		}
 	}
 	
 	public static void quit(Scanner console, int number){
 		System.out.print("\nQuit? ");
 		String quit = console.next();
 		if(quit.substring(0,1).toLowerCase().equals("q") || quit.substring(0,1).toLowerCase().equals("y")){
-			
+			number = 0;
 		} else if (quit.substring(0,1).toLowerCase().equals("n")){ 
 			System.out.println("Type a number from 1-100: ");
-			number = console.nextInt();
 			checkMismatch(console, number); 
 		}
 	}
