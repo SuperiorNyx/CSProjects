6c9f252dddef497badd408c117b844ae2b95b8eb
Rebecca Nicacio <rebecca_nicacio@yahoo.com>
2015-11-16 16:39:00 -0600

    Implemented iput for a second number & gcd

--- FirstProject/src/FirstProject.java
+++ FirstProject/src/FirstProject.java
@@ -2,28 +2,44 @@ import java.util.*;
 public class FirstProject {
 	public static void main(String[] args){
 		Scanner console = new Scanner(System.in);
-		System.out.println("Type a number from 1-100: ");
-		int number = 0;
-		checkMismatch(console, number);	
+		boolean run = true;
+		while(run){
+			System.out.print("Type a number from 1-100: ");
+			int number = checkMismatch(console, 0);
+			System.out.print("Do you want to enter a second number? ");
+			String yn = console.next();
+			if(yn.substring(0,1).toLowerCase().equals("y")){
+            System.out.print("Type a number from 1-100: ");
+				int number2 = checkMismatch(console, 0);
+            System.out.print("Greatest Common Divisor of " + number + " and " 
+                                 + number2 + " is: " + gcd(number, number2));
+            run = quit(console, run);
+			} else {
+				factor(number);
+				run = quit(console, run);
+			}
+		} 
 	}
 	
-	public static void checkMismatch(Scanner console, int number){
+	public static int checkMismatch(Scanner console, int number){
 		try{
 			number = Integer.parseInt(console.next());
 			if(number > 100 || number < 1){
-				System.out.println("Please input a number between 1 and 100: ");
-				checkMismatch(console, number);
+				System.out.print("Please input a number between 1 and 100: ");
+				number = checkMismatch(console, number);
 			} else {
-				factor(number);
-				quit(console, number);
+				number = (int) number;
+				return (int)number;
 			}
 		} catch(NumberFormatException e){
-			System.out.println("Only integer values allowed. Please input a number between 1 and 100: ");
-			checkMismatch(console, number);
+			System.out.print("Only integer values allowed. Please input a number between 1 and 100: ");
+			number = checkMismatch(console, number);
 		}
+		return (int)number;
 	}
 	
 	public static void factor(int number){
+      System.out.print("Factors of " + number + ": ");
 		for (int i = 1; i < number; i++){
 			if(number % i == 0){
 				System.out.print(i + " ");
@@ -45,14 +61,16 @@ public class FirstProject {
 		}
 	}
 	
-	public static void quit(Scanner console, int number){
+	public static boolean quit(Scanner console, boolean run){
 		System.out.print("\nQuit? ");
 		String quit = console.next();
 		if(quit.substring(0,1).toLowerCase().equals("q") || quit.substring(0,1).toLowerCase().equals("y")){
-			number = 0;
+			run = false;
+			return run;
 		} else if (quit.substring(0,1).toLowerCase().equals("n")){ 
-			System.out.println("Type a number from 1-100: ");
-			checkMismatch(console, number); 
+			run = true;
+			return run;
 		}
+		return run;
 	}
 }
